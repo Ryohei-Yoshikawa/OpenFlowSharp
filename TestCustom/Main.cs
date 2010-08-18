5cd54bc79bf49869f692a2468e83b710c8e6d139
Ryohei Yoshikawa <yoshikawa.ryohei@nowpro.co.jp>
2010-08-18 15:08:04 +0900

    初期化処理を変更。 とりあえずエミュレータでの動作は確認。

---
+++ TestCustom/Main.cs
@@ -0,0 +1,54 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace TestCustom
+{
+	public class Application
+	{
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args);
+		}
+	}
+
+	// The name AppDelegate is referenced in the MainWindow.xib file.
+	public partial class AppDelegate : MonoLib.Applet.MonoLibAppDelegate
+	{
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			// If you have defined a view, add it here:
+			// window.AddSubview (navigationController.View);
+
+			FramePerSeconds = 30;
+			StartMainLoop();
+
+			MonoLib.Core.TaskManager.Instance.InitTask(new TaskMain());
+			
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+
+		public override void Run()
+		{
+			MonoLib.Core.TaskManager.Instance.Proc();
+			MonoLib.Core.TaskManager.Instance.Draw();
+		}
+
+		public static AppDelegate Instance
+		{
+			get { return (AppDelegate)UIApplication.SharedApplication.Delegate; }
+		}
+
+		public UIWindow Window
+		{
+			get { return window; }
+		}
+	}
+}
+
---
+++ TestCustom/source/Tasks/TaskMain.cs
@@ -0,0 +1,84 @@
+﻿namespace TestCustom
+{
+	public class TaskMain : MonoLib.Core.Task, OpenFlowSharp.IOpenFlowDataSource
+	{
+		public override void Open()
+		{
+			InitializeImageList();
+
+			_view = new OpenFlowSharp.OpenFlowView()
+			{
+				Frame = AppDelegate.Instance.Window.Frame,
+				Bounds = AppDelegate.Instance.Window.Bounds,
+				BackgroundColor = MonoTouch.UIKit.UIColor.Blue,
+				DataSource = this
+			};
+			_view.ItemChanged += OnItemChanged;
+			_view.ItemPicked += OnItemPicked;
+			AppDelegate.Instance.Window.AddSubview(_view);
+			IsOpened = true;
+		}
+
+		public override void Close()
+		{
+			_view.ItemChanged -= OnItemChanged;
+			_view.ItemPicked -= OnItemPicked;
+			_view.RemoveFromSuperview();
+			IsClosed = true;
+		}
+
+		void OnItemChanged(OpenFlowSharp.OpenFlowView sender, int index)
+		{
+		}
+		void OnItemPicked(OpenFlowSharp.OpenFlowView sender, int index)
+		{
+		}
+
+		#region IOpenFlowDataSource メンバ
+
+		public int NumberOfImages(OpenFlowSharp.OpenFlowView openFlowView)
+		{
+			System.Diagnostics.Debug.WriteLine("+ [NumberOfImages]");
+			return _imageList.Count;
+		}
+
+		public MonoTouch.UIKit.UIImage RequestImage(OpenFlowSharp.OpenFlowView openFlowView, int index)
+		{
+			System.Diagnostics.Debug.WriteLine("+ [RequestImage] Index:" + index);
+			if (0 == index % 2)
+			{
+				return null;
+			}
+			return _imageList[index];
+		}
+
+		public MonoTouch.UIKit.UIImage RequestDefaultImage(OpenFlowSharp.OpenFlowView openFlowView)
+		{
+			System.Diagnostics.Debug.WriteLine("+ [RequestDefaultImage]");
+			return _defaultImage;
+		}
+
+		#endregion
+
+		/// <summary>
+		/// 表示用画像を用意する。
+		/// </summary>
+		private void InitializeImageList()
+		{
+			_defaultImage = MonoTouch.UIKit.UIImage.FromFile("default.png");
+			_imageList = new System.Collections.Generic.List<MonoTouch.UIKit.UIImage>(30);
+			int i = 0;
+			while (_imageList.Count < _imageList.Capacity)
+			{
+				MonoTouch.UIKit.UIImage img = MonoTouch.UIKit.UIImage.FromFile("images/" + i + ".jpg");
+				_imageList.Add(img);
+				++i;
+			}
+		}
+
+		private OpenFlowSharp.OpenFlowView _view;
+		private MonoTouch.UIKit.UIImage _defaultImage;
+		private System.Collections.Generic.List<MonoTouch.UIKit.UIImage> _imageList;
+	}
+}
+
